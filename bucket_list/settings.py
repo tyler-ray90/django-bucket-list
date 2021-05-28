a32386691fce26ed5120273c23de8f6022bc8e09
tyler-ray90 <tyler_ray98@yahoo.com>
2021-05-28 14:20:44 -0400

    added the migration for the bucketlist table

--- bucket_list/settings.py
+++ bucket_list/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'bucketlist.apps.BucketlistConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
