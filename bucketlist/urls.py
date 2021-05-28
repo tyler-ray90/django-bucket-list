2ab8b78f8464d3b7b3b7d9aec891fe0dc350da54
tyler-ray90 <tyler_ray98@yahoo.com>
2021-05-28 17:43:26 -0400

    added urls and updated index views

--- bucketlist/urls.py
+++ bucketlist/urls.py
@@ -2,5 +2,12 @@ from django.urls import path
 from . import views 
 
 urlpatterns = [
+    # ex: /bucketlist/
     path('', views.index, name='index'),
+    # ex: /bucketlist/5/
+    path('<int:question_id>/', views.detail, name='detail'),
+    # ex: /bucketlist/5/results/
+    path('<int:question_id>/results/', views.results, name='results'),
+    # ex: /bucketlist/5/vote/
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
