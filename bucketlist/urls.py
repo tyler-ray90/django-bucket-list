883b338d08768318c7e5cfcb0d97fc32b07bb9c9
tyler-ray90 <tyler_ray98@yahoo.com>
2021-05-31 18:19:38 -0400

    Updated views.py to generic views

--- bucketlist/urls.py
+++ bucketlist/urls.py
@@ -1,14 +1,11 @@
 from django.urls import path
-from . import views 
+
+from . import views
 
 app_name = 'bucketlist'
 urlpatterns = [
-    # ex: /bucketlist/
-    path('', views.index, name='index'),
-    # ex: /bucketlist/5/
-    path('<int:question_id>/', views.detail, name='detail'),
-    # ex: /bucketlist/5/results/
-    path('<int:question_id>/results/', views.results, name='results'),
-    # ex: /bucketlist/5/vote/
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
