d0110cc550115b484d052ba0fac309c3bab47226
tyler-ray90 <tyler_ray98@yahoo.com>
2021-06-03 16:23:05 -0400

    updated the Urls.py to show the correct path for heroku

--- bucket_list/urls.py
+++ bucket_list/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('bucketlist/', include('bucketlist.urls')),
+    path('', include('bucketlist.urls')),
     path('admin/', admin.site.urls),
 ]
